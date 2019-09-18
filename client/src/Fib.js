f2311a4e153f8947d79a18d8a76267046df49744
Misty Developer <mistydev@outlook.com>
2019-09-18 11:51:34 -0400

    minor changes

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -41,7 +41,7 @@ class Fib extends Component {
   renderValues() {
     const entries = [];
 
-    for (let key in this.state.values) {
+    for (const key in this.state.values) {
       entries.push(
         <div key={key}>
           For index {key} I calculated {this.state.values[key]}
