dd6df30229ac759e213cdbedabb4e81f461e56c9
Misty Developer <mistydev@outlook.com>
2019-10-17 16:40:53 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# Build
+docker build -t mistydev/multi-client:latest -t mistydev/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mistydev/multi-server:latest -t mistydev/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mistydev/multi-worker:latest -t mistydev/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# Push
+docker push mistydev/multi-client:latest
+docker push mistydev/multi-server:latest
+docker push mistydev/multi-worker:latest
+docker push mistydev/multi-client:$SHA
+docker push mistydev/multi-server:$SHA
+docker push mistydev/multi-worker:$SHA
+
+# Apply
+kubectl apply -f k8s
+kubectl set image deploymants/client-deployment server=mistydev/multi-client:$SHA
+kubectl set image deploymants/server-deployment server=mistydev/multi-server:$SHA
+kubectl set image deploymants/worker-deployment server=mistydev/multi-worker:$SHA
